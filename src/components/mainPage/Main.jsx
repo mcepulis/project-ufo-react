934781244678e73d32f367c167397b9d1bc3b204
mcepulis <109752570+mcepulis@users.noreply.github.com>
2024-03-20 16:45:07 +0200

    button fix

--- src/components/mainPage/Main.jsx
+++ src/components/mainPage/Main.jsx
@@ -7,7 +7,7 @@ function Main() {
             <div className={style.mainLeft}>
                 <h1 className={style.mainH1}>404</h1>
                 <p className={style.mainP}>Page not found</p>
-                <a className={style.mainA} href="/second-page">Go home</a>
+                <a className={style.mainA} href="/second-page">Second Page</a>
             </div>
             <img className={style.mainRight} src={pic} alt="Ufo lekste kuri vagia daiktus" />
         </main>
